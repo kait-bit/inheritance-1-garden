c90430f315ac60045b7588a9905e51ae88d4ca8e
kaiti <122762348+kait-bit@users.noreply.github.com>
2025-02-21 21:48:23 -0600

    changed problem up to add polymorphism

--- src/Tester.java
+++ src/Tester.java
@@ -1,10 +1,28 @@
+import java.util.ArrayList;
+
 public class Tester {
     public static void main(String[] args) {
+        ArrayList<Garden> g = new ArrayList<Garden>();
+
         FruitPatch f = new FruitPatch(20, "strawberries");
         System.out.println(f.water());
 
         VegPatch v = new VegPatch(10, "carrots");
         System.out.println(v.water());
-        System.out.println(v.shooRabbit());
+
+        VegPatch v2 = new VegPatch(5, "broccoli");
+        System.out.println(v2.water());
+
+        g.add(f);
+        g.add(v);
+        g.add(v2);
+
+        int totalPlants = 0;
+
+        for (Garden garden : g) {
+            totalPlants += garden.numPlants();
+        }
+
+        System.out.println("Total plants in all of your gardens: " + totalPlants);
     }
 }
