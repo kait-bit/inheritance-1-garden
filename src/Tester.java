5d0f726241f7bc0f836d7c440bdc9e1a3c59ac59
kaiti <122762348+kait-bit@users.noreply.github.com>
2025-02-22 11:17:43 -0600

    changed loop

--- src/Tester.java
+++ src/Tester.java
@@ -2,19 +2,11 @@ import java.util.ArrayList;
 
 public class Tester {
     public static void main(String[] args) {
-        ArrayList<Garden> g = new ArrayList<Garden>();
+        ArrayList<Garden> g = new ArrayList<>();
 
         FruitPatch f = new FruitPatch(20, "strawberries");
-        System.out.println(f.water());
-        System.out.println(f.toString());
-
         VegPatch v = new VegPatch(10, "carrots");
-        System.out.println(v.water());
-        System.out.println(v.toString());
-
         VegPatch v2 = new VegPatch(5, "broccoli");
-        System.out.println(v2.water());
-        System.out.println(v2.toString());
 
         g.add(f);
         g.add(v);
@@ -24,6 +16,8 @@ public class Tester {
 
         for (Garden garden : g) {
             totalPlants += garden.numPlants();
+            System.out.println(garden.water());
+            System.out.println(garden.toString());
         }
 
         System.out.println("Total plants in all of your gardens: " + totalPlants);
