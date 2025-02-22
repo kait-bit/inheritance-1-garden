2955c27c1a97a3755869b3ae20bb25ee7a0dac74
kaiti <122762348+kait-bit@users.noreply.github.com>
2025-02-21 21:47:03 -0600

    simplified

--- src/VegPatch.java
+++ src/VegPatch.java
@@ -2,12 +2,4 @@ public record VegPatch(int numPlants, String veg) implements Garden {
     @Override public String water() {
         return "You water " + numPlants + " " + veg + ".";
     }
-
-    public String shooRabbit() {
-        if (veg.equals("carrots") || veg.equals("Carrots")) {
-            return "The rabbit steals a carrot!";
-        } else {
-            return "You successfully shoo away the rabbit.";
-        }
-    }
 }
